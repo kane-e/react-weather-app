12d28c991c1648147bc9e0c6d266dd1b0831dc32
Emma Kane <kane2@pdx.edu>
2020-09-27 11:44:07 -0700

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import ReactAnimatedWeather from "react-animated-weather";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        <ReactAnimatedWeather icon="CLEAR_DAY" />
       </header>
     </div>
   );
