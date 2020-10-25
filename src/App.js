0954e487090d1979da6fd5d6e9e4db4f40ae2e9b
Emma Kane <kane2@pdx.edu>
2020-10-25 00:35:51 -0700

    Started building theme button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Search from "./Search";
+import palms from "./images/palms.jpg";
+import mountains from "./images/mountains.jpg"
+ 
 
 
 import Footer from "./Footer";
 
+
 export default function App() {
+  const [background, setBackground] = useState(`url(${mountains})`);
+
+  function changeBackground(event){
+    setBackground(`url(${palms})`);
+  }
   return (
-    <div className="App">
+    <div className="App background-image" style={{backgroundImage: background}}>
+      <button type="button" className="btn btn-dark theme-button" onClick={changeBackground}>Theme</button>
       <div className="app-wrapper">
         <div className="weather-app">
           <Search defaultCity="Tokyo" />
@@ -18,3 +28,5 @@ export default function App() {
     </div>
   );
 }
+
+// background image
