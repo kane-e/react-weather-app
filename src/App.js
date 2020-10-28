3b9b55b37b76abcd328e5dbffccaef02def264a6
Emma Kane <kane2@pdx.edu>
2020-10-27 17:09:54 -0700

    Began building theme button

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Search from "./Search";
-import palms from "./images/palms.jpg";
-import mountains from "./images/mountains.jpg"
- 
-
-
 import Footer from "./Footer";
 
 
 export default function App() {
-  const [background, setBackground] = useState(`url(${mountains})`);
+  const [backgroundColor, setBackgroundColor] = useState("linear-gradient(-225deg, #407088 0%, #5d54a4 90%)");
+  const [textColor, setTextColor] = useState("");
+  const [key, setKey] = useState("")
+
+  const colorArray = [
+    {
+      background: "linear-gradient(to right, #434343 0%, black 100%)",
+      text: "pink"
+    },
+    {
+      background: "linear-gradient(-60deg, #ff5858 0%, #f09819 100%)",
+      text: "blue"
+    },
+    {
+      background: "linear-gradient(to top, #0ba360 0%, #3cba92 100%)",
+      text: "purple"
+    }
+  ]
 
   function changeBackground(event){
-    setBackground(`url(${palms})`);
+    event.preventDefault();
+    colorArray.map((object, index) => {
+        return (
+        setBackgroundColor(colorArray.background),
+        setTextColor(colorArray.text),
+        setKey(colorArray.index)
+        )
+     }
+     
+     );
+
   }
   return (
-    <div className="App background-image" style={{backgroundImage: background}}>
-      <button type="button" className="btn btn-dark theme-button" onClick={changeBackground}>Theme</button>
+    <div className="App background-image" >
+      
       <div className="app-wrapper">
-        <div className="weather-app">
+       <button type="button" className="btn btn-dark theme-button" onClick={changeBackground}>Theme</button>
+        <div className="weather-app" style={{backgroundImage: backgroundColor, color: textColor}}>
           <Search defaultCity="Tokyo" />
         </div>
         <Footer />
